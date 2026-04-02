da99066ac53042ae99b0f48589471ae6a597c685
Emilia Skuba <skubaemilia@gmail.com>
2026-04-02 18:51:41 +0200

    branch issue-7 and class ArduinoBridge

--- src/serial_bridge.py
+++ src/serial_bridge.py
@@ -1,11 +1,21 @@
+import serial
+
 class ArduinoBridge:
     def __init__(self, port: str, baudrate: int = 115200, enabled: bool = True):
-
-        pass
+        self.port = port
+        self.baudrate = baudrate
+        self.enabled = enabled
+        self.ser=None
 
     def connect(self) -> None:
-
-        pass
+        if not self.enabled:
+            return
+        else:
+            try:
+                self.ser = serial.Serial(self.port, self.baudrate, timeout=1)
+                print(f"Connected to {self.port}")
+            except Exception as e:
+                print(f"Failed to connect to {self.port} : {e}")
 
     def send_joint_angles_deg(self, angles_deg) -> None:
 
