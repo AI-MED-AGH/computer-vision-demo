1ef61f22175f6390e741cb9de52a0e01c98dedcf
eszal <eszalka@student.agh.edu.pl>
2026-04-08 12:00:39 +0200

    added obj files, updated visualisation in drake_sim script.

--- src/supervisor.py
+++ src/supervisor.py
@@ -1,11 +1,37 @@
+from drake_sim import DrakeArmController
+import numpy as np
+import time
 
-
-
+#test modelu chwytaka
 class Supervisor:
     def __init__(self, config: dict):
-
-        pass
+        self.config = config
+        self.controller = DrakeArmController(
+            urdf_path=config["urdf_path"],
+            end_effector_frame_name=config["end_effector_frame_name"],
+            use_meshcat=config.get("use_meshcat", True),
+        )
 
     def run(self) -> None:
+        t = 0.0
+        dt = 0.03
 
-        pass
+        while True:
+            q = np.zeros(self.controller.num_positions)
+
+            if self.controller.num_positions > 0:
+                q[0] = 0.35 * np.sin(0.8 * t)
+            if self.controller.num_positions > 1:
+                q[1] = 0.25 * np.sin(1.0 * t + 0.4)
+            if self.controller.num_positions > 2:
+                q[2] = 0.20 * np.sin(1.2 * t + 0.8)
+            if self.controller.num_positions > 3:
+                q[3] = 0.18 * np.sin(0.9 * t + 1.1)
+            if self.controller.num_positions > 4:
+                q[4] = 0.15 * np.sin(1.1 * t + 1.5)
+
+            # reszta, czyli chwytak, zostaje na 0
+            self.controller.update_visualization(q)
+
+            time.sleep(dt)
+            t += dt
